e79809fb469ecb363365287e894893e8c5dc8653
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2024-10-09 06:39:01 +0600

    init: initialized the git repository
    This repository will mainly going to be used a template reference for testing out builtin j-tag interface of ESP32 S3

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+// Going to test out the builtin j-tag interface
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Starting...");
+    pinMode(LED_BUILTIN, OUTPUT);
+    delay(1000);
+}
+
+void loop() {
+    Serial.println("LED HIGH");
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("LED LOW");
+}
